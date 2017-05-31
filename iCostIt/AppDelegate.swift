835eb356f4d081752c65085a4d4f78a3832052eb
Allison Mcentire <allisonmcentire@gmail.com>
2017-05-31 14:40:27 -0700

    coffee disaster

--- iCostIt/AppDelegate.swift
+++ iCostIt/AppDelegate.swift
@@ -17,9 +17,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore  {
+            print("Not first launch.")
+        } else {
+            print("First launch, setting UserDefault.")
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            
+            let mainStoryboardIpad : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let initialViewControlleripad : UIViewController = mainStoryboardIpad.instantiateViewController(withIdentifier: "Tutorial") as UIViewController
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = initialViewControlleripad
+            self.window?.makeKeyAndVisible()
+            
+        }
         return true
     }
     
+   
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ iCostIt/TutorialPageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  TutorialPageViewController.swift
+//  iCostIt
+//
+//  Created by Allison Mcentire on 3/20/17.
+//  Copyright © 2017 com.millionacrenest. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    let pages = ["wageClasses1", "wageClasses2", "proposal1", "proposal2", "rollUp", "rollUp2"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.dataSource = self
+        
+        
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "wageClasses1")
+        setViewControllers([vc!], // Has to be a single item array, unless you're doing double sided stuff I believe
+            direction: .forward,
+            animated: true,
+            completion: nil)
+        
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let identifier = viewController.restorationIdentifier {
+            if let index = pages.index(of: identifier) {
+                if index > 0 {
+                    return self.storyboard?.instantiateViewController(withIdentifier: pages[index-1])
+                }
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let identifier = viewController.restorationIdentifier {
+            if let index = pages.index(of: identifier) {
+                if index < pages.count - 1 {
+                    return self.storyboard?.instantiateViewController(withIdentifier: pages[index+1])
+                }
+            }
+        }
+        return nil
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        if let identifier = viewControllers?.first?.restorationIdentifier {
+            if let index = pages.index(of: identifier) {
+                return index
+            }
+        }
+        return 0
+    }
+    
+    
+}
+
+
